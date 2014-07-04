8993031f60f976e16c7c1fce564ae2d5a7d062cd
denmar142 <dbuatan@gmail.com>
2014-07-04 02:17:19 -0700

    assignment_nku
    ni

---
+++ 3rd_Assignment/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _3rd_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(12, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(239, 63);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "0";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 96);
+            this.Controls.Add(this.btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Get Value";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn;
+
+    }
+}
+
---
+++ 3rd_Assignment/frmInputnumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3rd_Assignment
+{
+
+    public delegate void PassValueHandler(string strValue);
+
+    public partial class frmInputnumber : Form
+    {
+        public event PassValueHandler PassValue;
+
+        public frmInputnumber()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            int label = Convert.ToInt32(label1.Text);
+            int num1 = int.Parse(textBox1.Text);
+            int result = label + num1;
+            label1.Text = result.ToString();
+            if (PassValue != null)
+            {
+                PassValue(label1.Text);
+            }
+            
+           
+        }
+
+        
+
+  
+        
+    }
+}
